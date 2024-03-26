314c947c4dee6dad00dee10ea37ff4d2213fb9eb
Mirka2310 <gordinskamiroslava@gmail.com>
2024-03-26 20:08:50 +0100

    add

---
+++ src/components/App/App.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import Description from "../Description/Description";
+import Options from "../Options/Options";
+import Feedback from "../Feedback/Feedback";
+import Notification from "../Notification/Notification";
+
+const initFeedback = {
+	good: 0,
+	neutral: 0,
+	bad: 0,
+};
+
+function App() {
+	const [feedbacks, setFeedback] = useState(() => {
+		if (localStorage.getItem("feedbacks")) {
+			return JSON.parse(localStorage.getItem("feedbacks"));
+		} else {
+			return initFeedback;
+		}
+	});
+
+	const updateFeedback = (feedback) => {
+		switch (feedback) {
+			case "good":
+				setFeedback((prevFeedback) => ({
+					...prevFeedback,
+					good: prevFeedback.good + 1,
+				}));
+				break;
+			case "neutral":
+				setFeedback((prevFeedback) => ({
+					...prevFeedback,
+					neutral: prevFeedback.neutral + 1,
+				}));
+				break;
+			case "bad":
+				setFeedback((prevFeedback) => ({
+					...prevFeedback,
+					bad: prevFeedback.bad + 1,
+				}));
+				break;
+
+			default:
+				throw new Error("feedback type not found");
+		}
+	};
+
+	const resetFeedback = () => {
+		setFeedback(initFeedback);
+	};
+
+	useEffect(() => {
+		localStorage.setItem("feedbacks", JSON.stringify(feedbacks));
+	}, [feedbacks]);
+
+	const totalFeedback = Object.values(feedbacks).reduce(
+		(sum, feedback) => sum + feedback,
+		0,
+	);
+
+	const positiveFeedbackPercentage =
+		totalFeedback > 0
+			? Math.round(((feedbacks.good + feedbacks.neutral) / totalFeedback) * 100)
+			: 0;
+
+	return (
+		<>
+			<Description />
+			<Options
+				totalFeedback={totalFeedback}
+				updateFeedback={updateFeedback}
+				resetFeedback={resetFeedback}
+			/>
+			{totalFeedback > 0 ? (
+				<Feedback
+					feedbacks={feedbacks}
+					totalFeedback={totalFeedback}
+					positiveFeedbackPercentage={positiveFeedbackPercentage}
+				/>
+			) : (
+				<Notification />
+			)}
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,15 @@
+const Feedback = ({ feedbacks, totalFeedback, positiveFeedbackPercentage }) => {
+    return (
+      <div>
+        <ul>
+          <li>Good: {feedbacks.good}</li>
+          <li>Neutral: {feedbacks.neutral}</li>
+          <li>Bad: {feedbacks.bad}</li>
+          <li>Total: {totalFeedback}</li>
+          <li>Positive: {positiveFeedbackPercentage}%</li>
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Feedback;
